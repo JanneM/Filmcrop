cd20751acb75b29d4400c99c339dd69a85f2a86f
Jan Moren <jan.moren@gmail.com>
2012-01-26 15:32:56 +0900

    more cleaning

--- filmcrop.rb
+++ filmcrop.rb
@@ -2,13 +2,23 @@
 require 'RMagick'
 require 'optparse'
 require 'pp'
-ds=0
-diff_fac=0.1
 
+$diff_fac=0.1	# fraction frame width to search for an edge
+$margin=1.0/8	# ignored top and bottom area
+$ds=nil		# scale factor for the small images we work with
+# Get a list of input files, optionally an output prefix (defaulting to
+# "crop").
+
+fheader="crop"
+images=6
+
+dofit=true
+fitsize=0
+doaverage=true
 # create the intensity array corresponding to a given strip
-# margin is how much to ignore toward the edges
+# $margin is how much to ignore toward the edges
 
-def make_intense(fstrip, margin)
+def make_intense(fstrip)
 
     rows=fstrip.rows
     cols =fstrip.columns
@@ -18,7 +28,7 @@ def make_intense(fstrip, margin)
     minp = 1000000
 
     cols.times {|x|
-	pixels = fstrip.get_pixels(x,(rows*margin),1,(rows-rows*margin))
+	pixels = fstrip.get_pixels(x,(rows*$margin),1,(rows-rows*$margin))
 	mp=0.0
 	pixels.each { |p|
 	    val=p.intensity()
@@ -34,15 +44,15 @@ end
 
 # Find the minima in the neighbourhood around each possible separation point
 
-def find_minima(arr, seps, nhood)
-    pstep = arr.length/(seps+1)
-    diffstep = Integer(pstep*nhood)
+def find_minima(arr, frames)
+    pstep = arr.length/frames
+    diffstep = Integer(pstep*$diff_fac)
 
-    mins=Array.new(seps+2)
+    mins=Array.new(frames+1)
     mins[0]=0
-    mins[seps+1]=arr.length-1
+    mins[frames]=arr.length-1
 
-    seps.times {|sep|
+    (frames-1).times {|sep|
 	pc = (sep+1)*pstep
 	min=100000
 	finp=-1
@@ -58,12 +68,11 @@ def find_minima(arr, seps, nhood)
 end
 
 # find least square minima
-def find_sqr_minima(strip, frames, avgframe, nhood)
+def find_sqr_minima(strip, frames, avgframe)
     arr = strip.intensity
-    ds = strip.divs
     print "frames: ", frames, "\n"
     pstep = arr.length/(frames)
-    diffstep = Integer(pstep*nhood)
+    diffstep = Integer(pstep*$diff_fac)
 
     mins=Array.new(frames)
 
@@ -92,22 +101,13 @@ def find_sqr_minima(strip, frames, avgframe, nhood)
 		end 
 	    }
 
-	    mins[fr]=[(ps+finp)*ds, (pe+finp)*ds]
+	    mins[fr]=[(ps+finp)*$ds, (pe+finp)*$ds]
     }
-    mins[0]=[(mins[1][0]-avgframe*ds), (mins[1][0])]
-    mins[frames-1]=[mins[frames-2][1], mins[frames-2][1]+avgframe*ds]
+    mins[0]=[(mins[1][0]-avgframe*$ds), (mins[1][0])]
+    mins[frames-1]=[mins[frames-2][1], mins[frames-2][1]+avgframe*$ds]
     mins
 end
 
-# Get a list of input files, optionally an output prefix (defaulting to
-# "crop").
-
-fheader="crop"
-images=6
-margin=1.0/8
-dofit=true
-fitsize=0
-doaverage=true
 
 optparse = OptionParser.new { |opts|
 
@@ -158,7 +158,6 @@ end
 #fheader=File.basename(infile, File.extname(infile))
 
 Img = Struct.new(:fname,	# Full size file name
-		 :divs,		# scale divisor
 		 :intensity,	# average value array
 		 :cuts,		# found cut positions
 		 :fcut,		# final frame cuts
@@ -169,15 +168,15 @@ files.each {|infile|
     print "Processing file: ", infile, "\n"
     instrip = Magick::Image.read(infile).first
     instrip.rotate!(-90, '<')
-    ds=instrip.rows/400.0
-    strip=instrip.resize(1/ds)
+    $ds=$ds||instrip.rows/200.0
+    strip=instrip.resize(1/$ds)
     instrip.destroy!
 
-    arr=make_intense(strip, margin)
-    cuts=find_minima(arr, seps, diff_fac)
+    arr=make_intense(strip)
+    cuts=find_minima(arr, images)
     
     a=Array.new()
-    strips.push(Img.new(infile, ds, arr, cuts,a, strip.rows, strip.columns))
+    strips.push(Img.new(infile, arr, cuts,a, strip.rows, strip.columns))
     strip.destroy!
 }
 
@@ -189,34 +188,31 @@ files.each {|infile|
 # fit to an average or given frame width 
 if dofit
     print "fitting... \n"
-
-    total=0
-    mintotal=0
-    nr=0
-    strips.each {|s|
-	if doaverage		    # find average frame size
-	    (1..(seps-1)).each {|i|	    # note '1'
+    avg=0
+    if doaverage		    # find average frame size
+	total=0
+	nr=0
+	strips.each {|s|
+	    (1..(images-2)).each {|i|	    # note '1'
 		total+=(s.cuts[i+1]-s.cuts[i])
 		nr+=1
 	    }
-	else			    # use user-supplied size
-	    total+=fitsize/s.divs
-	    nr+=1
-	end
-    }
-#    print "Average: ", total,"/", nr, " = ", total/nr, "\n"
-    avg = Integer(total/nr)
+	}
+	#    print "Average: ", total,"/", nr, " = ", total/nr, "\n"
+	avg = Integer(total/nr)
+    else			    # use user-supplied size
+	avg=fitsize/$ds
+    end
 
     strips.each {|s|
-	f2=find_sqr_minima(s, images, avg, diff_fac)
-	s.fcut = f2
+	s.fcut=find_sqr_minima(s, images, avg)
     }
 
 # No fixed frame, so just set the cuts to each local minimum
 else			
     strips.each {|s|
-	(0..(seps)).each {|i|
-	    f=[s.cuts[i]*s.divs, s.cuts[i+1]*s.divs]
+	(0..(images-1)).each {|i|
+	    f=[s.cuts[i]*$ds, s.cuts[i+1]*$ds]
 	    s.fcut.push(f)
 	}
     }
@@ -228,9 +224,8 @@ strips.each {|s|
     print "crop file: ", s.fname, "\n"
     instrip = Magick::Image.read(s.fname).first
     instrip.rotate!(-90, '<')
-    ds=s.divs
 
-    (0..(seps)).each {|i|
+    (0..(images-1)).each {|i|
 	cuts=s.fcut[i]
 
 	outimg=instrip.crop(cuts[0],0,(cuts[1]-cuts[0]),instrip.rows,true)
@@ -239,33 +234,8 @@ strips.each {|s|
 	outimg.write(fname)
 	print "\t f# ", i, "\n"
 	outimg.destroy!
-#	print "size: ", (cuts[1]-cuts[0]),"\n"
     }
     instrip.destroy!
 }
 
-exit()
-
 	
-pix=Array.new(1)
-pix[0]=Magick::Pixel.from_color('yellow')
-cols.times {|x|
-    v=acc[x]*rows*0.9
-    filmstrip.store_pixels(x,(rows-v),1,1,pix)}
-
-pix[0]=Magick::Pixel.from_color('cyan')
-mins.each {|p|
-    filmstrip.store_pixels(p/ds,rows-1,1,1,pix)}
-
-#filmstrip.view(0,0,cols,rows) do |view|
-#    acc.each_with_index {|v,i| view[v][i]='yellow'}
-#end
-
-filmstrip.write("testout.tif")
-#cols.times {|x|
-#    acc
-
-#printf("Max: %f	\t min: %f\n", maxp, minp)
-
-
-
